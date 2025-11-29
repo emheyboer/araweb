6dbaa600569c1599fbdf5084d62191b6c54c897e
Emily H <157163774+emheyboer@users.noreply.github.com>
2025-11-29 00:22:14 -0500

    card class

--- public/main.js
+++ public/main.js
@@ -4,17 +4,67 @@ async function setup() {
     const output = document.getElementById('output');
 
     const cards = [
-        ['right now', '/latest'],
-        ['outside right now', '/outside'],
-        ['yesterday', '/yesterday'],
-        ['one month ago', '/lastmonth'],
-        ['one year ago', '/lastyear'],
+        ['right now', 'latest', '/latest'],
+        ['outside right now', 'outside', '/outside'],
+        ['yesterday', 'yesterday', '/yesterday'],
+        ['one month ago', 'lastmonth', '/lastmonth'],
+        ['one year ago', 'lastyear', '/lastyear'],
     ];
 
     for (let i = 0; i < cards.length; i++) {
-        const [title, endpoint] = cards[i];
-        const card = newCard(title, endpoint);
-        output.appendChild(card);
+        const [title, id, endpoint] = cards[i];
+        const card = new Card(title, id, endpoint)
+        output.appendChild(card.div);
+    }
+}
+
+class Card {
+    constructor(title, id, endpoint) {
+        this.title = title;
+        this.id = id;
+        this.endpoint = endpoint;
+
+        this.initDiv();
+
+        const keys = ['co2', 'temperature', 'humidity', 'pressure'];
+        this.initRows(keys);
+
+        this.update();
+    }
+
+    initDiv() {
+        this.div = document.createElement('div');
+        this.div.id = this.id;
+        this.div.classList.add('card');
+
+        const span = document.createElement('span');
+        span.id = `${this.id}-title`;
+        span.innerText = this.title;
+        this.div.appendChild(span);
+        span.classList.add('title');
+    }
+
+    initRows(keys) {
+        this.rows = [];
+        keys.forEach(key => {
+            const span = document.createElement('span');
+            span.id = `${this.id}-${key}`;
+            span.classList.add('row', key);
+            this.rows.push({ key, span });
+            this.div.appendChild(span);
+        })
+    }
+
+    async update() {
+        const reading = await retryFetch(`${api_url}${this.endpoint}`).then(res => res.json());
+
+        this.rows.forEach(row => {
+            row.span.innerHTML = formatValue(row.key, reading[row.key]);
+        })
+
+        setTimeout(() => {
+            this.update();
+        }, 1000);
     }
 }
 
@@ -28,44 +78,26 @@ function retryFetch(resource, options, backoff = 500) {
     return fetch(resource, options).catch(onError);
 }
 
-function newCard(title, endpoint) {
-    const card = document.createElement('div');
-    card.id = `${title}-card`;
-    card.classList.add('card');
-    const update = async () => {
-        const reading = await retryFetch(`${api_url}${endpoint}`).then(res => res.json());
-        card.innerHTML = formatReading(title, reading);
+function formatValue(key, value) {
+    let display = value.toLocaleString();
+    switch (key) {
+        case 'date':
+            display = new Date(value).toLocaleString();
+            return;
+        case 'co2':
+            display = `<span style="color: ${co2Color(value)}">${display}</span> ppm co2`;
+            break;
+        case 'temperature':
+            display = `${display}°F`;
+            break;
+        case 'humidity':
+            display = `${display}% humidity`;
+            break;
+        case 'pressure':
+            display = `${display} hPa`;
+            break;
     }
-    update();
-    setInterval(update, 1000);
-    return card;
-}
-
-function formatReading(title, reading) {
-    result = `<span id="${title}-title">${title}</span>`;
-    Object.entries(reading).forEach(([key, value]) => {
-        let display = value.toLocaleString();
-        switch (key) {
-            case 'date':
-                display = new Date(value).toLocaleString();
-                return;
-            case 'co2':
-                display = `<span style="color: ${co2Color(value)}">${display}</span> ppm co2`;
-                break;
-            case 'temperature':
-                display = `${display}°F`;
-                break;
-            case 'humidity':
-                display = `${display}% humidity`;
-                break;
-            case 'pressure':
-                display = `${display} hPa`;
-                break;
-        }
-
-        result += `<span id="${title}-${key}">${display}</span>`
-    });
-    return result;
+    return display;
 }
 
 function co2Color(co2) {
